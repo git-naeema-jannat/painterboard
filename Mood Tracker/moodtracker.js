162930ff401828ead30f7a0544c0e7e0b45fe5ed
git -naeema-jannat <naeemajannat375@gmail.com>
2025-05-28 10:03:20 +0000

    basic click and function done, working toward making responsible and automatic days creation depending on month, also restricting changing prev mode

--- Mood Tracker/moodtracker.js
+++ Mood Tracker/moodtracker.js
@@ -1,6 +1,29 @@
 var month = document.getElementsByClassName('month')[0];
 var days = document.getElementsByClassName('days')[0];
-
+var n = 0;
+var colorarr = ['#C91411', '#F9CC29', '#FA6A10', '#03BE18', '#F8AECA'];
+var moodarr = ['Angry', 'Meh', 'Excited', 'Happy', 'Calm']
+var date = new Date();
+var today = date.getDate();
 month.addEventListener('click', (event)=>{
-    event.target.style.background = 'red';
+    //console.log(event.target.querySelector('p').innerHTML)
+    if(today == event.target.querySelector('p').innerHTML){
+        
+        event.target.querySelectorAll('h2').forEach(element => {
+        element.remove();
+    });
+    event.target.style.background = colorarr[n];
+    let h2 = document.createElement('h2');
+    h2.innerHTML = moodarr[n];
+    event.target.appendChild(h2);
+    n++;
+    if(n === colorarr.length){
+        n = 0;
+    }
+}else{
+    
+        
+   
+}
+    
 })
