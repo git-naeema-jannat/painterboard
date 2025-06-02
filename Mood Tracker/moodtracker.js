b0fd88705dc083dfa1e0ae2e6c565449365ac7df
git -naeema-jannat <naeemajannat375@gmail.com>
2025-06-02 09:44:37 +0000

    dk.. just messing up

--- Mood Tracker/moodtracker.js
+++ Mood Tracker/moodtracker.js
@@ -20,10 +20,36 @@ month.addEventListener('click', (event)=>{
     if(n === colorarr.length){
         n = 0;
     }
-}else{
-    
-        
-   
-}
-    
+}    
 })
+
+const months = [
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+var now = date.getMonth();
+var number = now;
+console.log(months[5]);
+document.getElementsByClassName('month-name')[0].innerHTML = months[now];
+function prevMonth(){
+    number = number -1;
+    document.getElementsByClassName('month-name')[0].innerHTML = months[number];
+    if (number === 0) {
+  number = 11;
+  //year--; // optional
+}
+else {
+  number--;
+}
+}
+function nextMonth(){
+    number = number + 1;
+    document.getElementsByClassName('month-name')[0].innerHTML = months[number];
+    if (number === months.length - 1) {
+  number = 0;
+ // year++; // optional if you're tracking year
+}
+else {
+  number++;
+}
+}
